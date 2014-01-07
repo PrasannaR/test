a88ddc2efb87fc3ff14ada75627aaeb58a252ecc
Prasanna <Prasanna.R4@cognizant.com>
2014-01-07 15:28:20 +0530

    Removed Old Revison Files

--- trumobiconnect2.3_updated/src/com/cognizant/trumobi/persona/net/PersonaCertificateDownloadRequest.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.trumobi.persona.net;
-
-import com.google.gson.annotations.SerializedName;
-
-public class PersonaCertificateDownloadRequest {
-
-	@SerializedName("certificate_request_id")
-	public String certificate_request_id;
-}
