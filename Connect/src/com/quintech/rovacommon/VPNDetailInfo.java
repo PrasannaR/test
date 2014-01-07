293ca7b84d0b37fb1ff01f0ff29d37bd4645877c
Prasanna <Prasanna.R4@cognizant.com>
2014-01-07 10:36:25 +0530

    Removed Old Revison Files

--- Connect/src/com/quintech/rovacommon/VPNDetailInfo.java
+++
@@ -1,9 +0,0 @@
-package com.quintech.rovacommon;
-
-public class VPNDetailInfo {
-	   public String VPNServer;
-	   public String VPNUsername;
-	   public String VPNPassword;
-	   public String VPNPort;
-	   public String VPNName;
-}
