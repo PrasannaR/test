fe3182500bff2c481d9450fa5b90a5acf82d3a93
Prasanna <Prasanna.R4@cognizant.com>
2014-01-07 11:28:11 +0530

    Removed Old Revison Files

--- trumobiconnect2.3_updated/src/com/cognizant/trumobi/em/service/EmEmailServiceConstants.java
+++
@@ -1,9 +0,0 @@
-
-
-package com.cognizant.trumobi.em.service;
-
-public class EmEmailServiceConstants {
-    public static final int MEETING_REQUEST_ACCEPTED = 1;
-    public static final int MEETING_REQUEST_TENTATIVE = 2;
-    public static final int MEETING_REQUEST_DECLINED = 3;
-}
